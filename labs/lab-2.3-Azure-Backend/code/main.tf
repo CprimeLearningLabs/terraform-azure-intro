afcde71a9370a33c04f1f5da59c7ffc6fcbdacbc
Peter Panec <peter.panec@cprime.com>
2020-10-08 16:06:49 -0700

    lab 2.3 material

---
+++ labs/lab-2.3-Azure-Backend/code/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 2.3.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "terraform-course-backend"
+    container_name       = "tfstate"
+    key                  = "cprime.terraform.labs.tfstate"
+  }
+  required_version = "~> 0.13.0"
+}
+
+provider "random" {
+}
+
+resource "random_integer" "number" {
+  min     = 1
+  max     = 100
+}
