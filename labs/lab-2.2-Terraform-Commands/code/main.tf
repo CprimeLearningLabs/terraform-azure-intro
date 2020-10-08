2b9086a7df0b2ec9e70f0c425f2a85c5d507f14b
Peter Panec <peter.panec@cprime.com>
2020-10-08 12:06:46 -0700

    add lab 2.2 material

---
+++ labs/lab-2.2-Terraform-Commands/code/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 2.3.0"
+    }
+  }
+  required_version = "~> 0.13.0"
+}
+
+provider "random" {
+}
+
+resource "random_integer" "number" {
+  min     = 1
+  max     = 100
+}
