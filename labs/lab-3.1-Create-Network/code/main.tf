e09d0853002bbceae808e68f52d748f8176ae3a1
Peter Panec <peter.panec@cprime.com>
2020-10-08 17:29:17 -0700

    lab 3.1 material

---
+++ labs/lab-3.1-Create-Network/code/main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.20, < 3.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "terraform-course-backend"
+    container_name       = "tfstate"
+    key                  = "cprime.terraform.labs.tfstate.0137"
+  }
+  required_version = "~> 0.13.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "lab" {
+  name     = "aztf-labs-rg"
+  location = "westus2"
+  tags     = {
+    Environment = "Lab"
+    Project     = "AZTF Training"
+  }
+}
+
+resource "azurerm_virtual_network" "lab" {
+  name                = "aztf-labs-vnet"
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.lab.name
+  address_space       = ["10.0.0.0/16"]
+  tags                = {
+    Environment = "Lab"
+    Project     = "AZTF Training"
+  }
+}
+
+resource "azurerm_subnet" "lab-public" {
+  name                 = "aztf-labs-subnet-public"
+  resource_group_name  = azurerm_resource_group.lab.name
+  virtual_network_name = azurerm_virtual_network.lab.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_subnet" "lab-private" {
+  name                 = "aztf-labs-subnet-private"
+  resource_group_name  = azurerm_resource_group.lab.name
+  virtual_network_name = azurerm_virtual_network.lab.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "lab-public" {
+  name                = "aztf-labs-public-sg"
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.lab.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "lab-public" {
+  subnet_id                 = azurerm_subnet.lab-public.id
+  network_security_group_id = azurerm_network_security_group.lab-public.id
+}
