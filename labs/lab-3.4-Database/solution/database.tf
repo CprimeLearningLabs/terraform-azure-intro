53f9925a199a5f1570c9ee64fe03a528e0b24001
Peter Panec <peter.panec@cprime.com>
2020-10-09 10:58:47 -0700

    lab 3.4 material

---
+++ labs/lab-3.4-Database/solution/database.tf
@@ -0,0 +1,37 @@
+resource "random_integer" "suffix" {
+  min = 10000
+  max = 99999
+}
+
+resource "azurerm_postgresql_server" "lab" {
+  name                = "aztf-labs-psqlserver-${random_integer.suffix.result}"
+  location            = local.region
+  resource_group_name = azurerm_resource_group.lab.name
+
+  sku_name                      = "B_Gen5_1"
+  version                       = "11"
+  storage_mb                    = 5120
+  public_network_access_enabled = true
+  ssl_enforcement_enabled       = false
+
+  administrator_login           = "psqladmin"
+  administrator_login_password  = "AZtfl4b$"
+
+  tags = local.common_tags
+}
+
+resource "azurerm_postgresql_database" "lab" {
+  name                = "aztf-labs-db"
+  resource_group_name = azurerm_resource_group.lab.name
+  server_name         = azurerm_postgresql_server.lab.name
+  charset             = "UTF8"
+  collation           = "English_United States.1252"
+}
+
+resource "azurerm_postgresql_firewall_rule" "lab" {
+  name                = "aztf-labs-fwrule"
+  resource_group_name = azurerm_resource_group.lab.name
+  server_name         = azurerm_postgresql_server.lab.name
+  start_ip_address    = cidrhost(azurerm_subnet.lab-private.address_prefixes[0],0)
+  end_ip_address      = cidrhost(azurerm_subnet.lab-private.address_prefixes[0],255)
+}
