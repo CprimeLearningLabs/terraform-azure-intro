d4ff8bda20d4283e57a17d53f976ee35c90b9d61
ppaneccprime <63671964+ppaneccprime@users.noreply.github.com>
2022-03-03 07:39:31 -0800

    change storage backend (#16)
    * update azurerm_lb_rule per azurerm change
    
    * update terraform version in code
    
    * update storage account name

--- Backend-Setup/storage/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "location" {
-  description = "Region in which to create the backend storage accounts."
-  type = string
-}
-
-variable "sequence" {
-  description = "Two-digit sequence number for this subscription to append to storage account name."
-  type = string
-}
--- Backend-Setup/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "subscription-ids" {
-  description = "Map of subscriptions ids, where the key is the two-digit student sequence number for the subscription."
-  type = map(string)
-}
-
-variable "location" {
-  description =  "Region in which to create the backend storage accounts."
-  type = string
-}
