e1cd7a205369a81b7614d1897af41f462b8e6dde
kishorerose <88154734+kishorerose@users.noreply.github.com>
2022-08-24 11:16:47 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 2.3.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+provider "random" {
+}
